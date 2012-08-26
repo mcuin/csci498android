5bc386ca9e7c343459b567c94acd7d0dd171fcac
Mykal Cuin <mcuin@mines.edu>
2012-08-26 17:13:22 -0600

    Changed functionality of HelloAndroid from saying hello and my name to giving the time each time the button on screen is pressed.

--- HelloAndroid/src/csci498/hello/HelloAndroid.java
+++ HelloAndroid/src/csci498/hello/HelloAndroid.java
@@ -2,19 +2,28 @@ package csci498.hello;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import java.util.Date;
 
-public class HelloAndroid extends Activity {
+public class HelloAndroid extends Activity implements View.OnClickListener {
+	Button hello;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hello_android);
+        hello = new Button(this);
+        hello.setOnClickListener(this);
+        updateHello();
+        setContentView(hello);
+    }
+    
+    public void onClick(View view) {
+    	updateHello();
+    }
+    
+    private void updateHello() {
+    	hello.setText(new Date().toString());
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_hello_android, menu);
-        return true;
-    }
 }
