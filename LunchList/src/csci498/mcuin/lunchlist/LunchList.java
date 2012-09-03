579ac61121f7214d036813810e162d8cadf1114e
Mykal Cuin <mcuin@mines.edu>
2012-09-02 18:42:29 -0600

    Finished all steps in chapter 2. Added Resturant.java and updated LunchList.java to specification. Starting on extra credit.

--- LunchList/src/csci498/mcuin/lunchlist/LunchList.java
+++ LunchList/src/csci498/mcuin/lunchlist/LunchList.java
@@ -2,19 +2,30 @@ package csci498.mcuin.lunchlist;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LunchList extends Activity {
+	
+	Resturant r = new Resturant();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lunch_list);
+        
+        Button save = ( Button )findViewById( R.id.save );
+        save.setOnClickListener( onSave );
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_lunch_list, menu);
-        return true;
-    }
-}
+    private View.OnClickListener onSave = new View.OnClickListener() {
+		public void onClick(View v) {
+			EditText name = ( EditText )findViewById( R.id.name );
+			EditText address = ( EditText )findViewById( R.id.addr );
+					
+			r.setName( name.getText().toString() );
+			r.setAddress( address.getText().toString() );
+		}
+	};
+}    
---
+++ LunchList/src/csci498/mcuin/lunchlist/Resturant.java
@@ -0,0 +1,22 @@
+package csci498.mcuin.lunchlist;
+
+public class Resturant {
+	private String name = "";
+	private String address = "";
+	
+	public String getName() {
+		return( name );
+	}
+	
+	public void setName( String name ) {
+		this.name = name;
+	}
+	
+	public String getAddress() {
+		return( address );
+	}
+	
+	public void setAddress( String address ) {
+		this.address = address;
+	}
+}
