c1dacefb9aed4d07cb9757f76f9cd36871e8ba2f
Mykal Cuin <mcuin@mines.edu>
2012-09-05 09:23:53 -0600

    Finished extra credit chapter 4. Added spinner and text complete with a list of cities in Colorado.

--- LunchList/src/csci498/mcuin/lunchlist/LunchList.java
+++ LunchList/src/csci498/mcuin/lunchlist/LunchList.java
@@ -7,16 +7,21 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RadioGroup;
 import android.widget.RadioButton;
+import android.widget.Spinner;
 
 public class LunchList extends Activity {
 	
 	List<Restaurant> model = new ArrayList<Restaurant>();
 	ArrayAdapter<Restaurant> adapter = null;
+	AutoCompleteTextView address;
+	ArrayAdapter<String> autoAdapter = null;
+	private String[] addresses;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,10 +31,18 @@ public class LunchList extends Activity {
         Button save = ( Button )findViewById( R.id.save );
         save.setOnClickListener( onSave );
         
-        ListView list= (ListView)findViewById( R.id.resturants );
+        Spinner list= (Spinner)findViewById( R.id.resturants );
         adapter = new ArrayAdapter<Restaurant>( this, android.R.layout.simple_list_item_1, model );
         
         list.setAdapter( adapter );
+        
+        addresses = new String[] {
+        		"Denver", "Colorado Spring", "Boulder", "Golden"
+        };
+        autoAdapter = new ArrayAdapter<String>( this, android.R.layout.simple_dropdown_item_1line, addresses );
+        
+        address = ( AutoCompleteTextView )findViewById( R.id.addr );
+        address.setAdapter( autoAdapter );
     }
 
     private View.OnClickListener onSave = new View.OnClickListener() {
