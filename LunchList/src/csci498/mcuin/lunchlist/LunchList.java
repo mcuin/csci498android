e83776e42c33ba766b9e6e9e7aa1353462c8874b
Mykal Cuin <mcuin@mines.edu>
2012-09-06 18:54:54 -0600

    Completed chapter 6 tutorial. The app works as expected and the files match the full file views given in the tutorial.

--- LunchList/src/csci498/mcuin/lunchlist/LunchList.java
+++ LunchList/src/csci498/mcuin/lunchlist/LunchList.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.graphics.Color;
+import android.app.TabActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
@@ -16,16 +16,17 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RadioGroup;
-import android.widget.Spinner;
+import android.widget.TabHost;
 import android.widget.TextView;
 
-public class LunchList extends Activity {
+public class LunchList extends TabActivity {
 	
 	List<Restaurant> model = new ArrayList<Restaurant>();
 	RestaurantAdapter adapter = null;
-	AutoCompleteTextView address;
-	ArrayAdapter<String> autoAdapter = null;
-	private String[] addresses;
+	EditText name = null;
+    EditText address = null;
+    RadioGroup types = null;
+    
 	
 	
 
@@ -34,34 +35,43 @@ public class LunchList extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lunch_list);
         
+	    name = ( EditText )findViewById( R.id.name );
+		address = ( EditText )findViewById( R.id.addr );
+		types = ( RadioGroup )findViewById( R.id.types );
+        
         Button save = ( Button )findViewById( R.id.save );
         save.setOnClickListener( onSave );
         
-        ListView list= (ListView)findViewById( R.id.resturants );
+        ListView list = (ListView)findViewById( R.id.restaurants );
         
         adapter = new RestaurantAdapter();
         
         list.setAdapter( adapter );
         
-        addresses = new String[] {
-        		"Denver", "Colorado Spring", "Boulder", "Golden"
-        };
-        autoAdapter = new ArrayAdapter<String>( this, android.R.layout.simple_dropdown_item_1line, addresses );
+        TabHost.TabSpec spec = getTabHost().newTabSpec( "tag1" );
         
-        address = ( AutoCompleteTextView )findViewById( R.id.addr );
-        address.setAdapter( autoAdapter );
+        spec.setContent( R.id.restaurants );
+        spec.setIndicator( "List", getResources().getDrawable( R.drawable.list ) );
+        
+        getTabHost().addTab( spec );
+        
+        spec = getTabHost().newTabSpec( "tag2" );
+        spec.setContent( R.id.details );
+        spec.setIndicator( "Details", getResources().getDrawable( R.drawable.restaurant ) );
+        
+        getTabHost().addTab( spec );
+        
+        getTabHost().setCurrentTab(0); 
+        
+        list.setOnItemClickListener( onListClick );
     }
 
-    private View.OnClickListener onSave = new View.OnClickListener() {
+	private View.OnClickListener onSave = new View.OnClickListener() {
 		public void onClick(View v) {
 			Restaurant r = new Restaurant();
-			EditText name = ( EditText )findViewById( R.id.name );
-			EditText address = ( EditText )findViewById( R.id.addr );
-					
 			r.setName( name.getText().toString() );
 			r.setAddress( address.getText().toString() );
 			
-			RadioGroup types = ( RadioGroup )findViewById( R.id.types );
 				
 				switch ( types.getCheckedRadioButtonId() ) {
 				  case R.id.sit_down:
@@ -78,6 +88,27 @@ public class LunchList extends Activity {
 			}
 	};
 	
+	private AdapterView.OnItemClickListener onListClick = new AdapterView.OnItemClickListener() {
+		public void onItemClick( AdapterView<?> parent, View view, int position, long id) {
+			Restaurant r = model.get( position );
+			
+			name.setText( r.getName() );
+			address.setText( r.getAddress() );
+			
+			if( r.getType().equals("sit_down")) {
+				types.check( R.id.sit_down );
+			}
+			else if( r.getType().equals("take_out")) {
+				types.check( R.id.take_out );
+			}
+			else {
+				types.check( R.id.delivery );
+			}
+			
+			getTabHost().setCurrentTab(1);
+		}
+	};
+	
 	class RestaurantAdapter extends ArrayAdapter<Restaurant> {
 		RestaurantAdapter() {
 			super( LunchList.this, android.R.layout.simple_list_item_1, model );
@@ -85,54 +116,30 @@ public class LunchList extends Activity {
 		
 		public View getView( int position, View convertView, ViewGroup parent ) {
 			View row = convertView;
-			RestaurantHolder holder = null;
 			
 			if( row == null ) {
 				LayoutInflater inflater = getLayoutInflater();
 				row = inflater.inflate( R.layout.row, null);
-				holder = new RestaurantHolder( row );
-				row.setTag( holder );
-			}
-			else {
-				holder = ( RestaurantHolder )row.getTag();
 			}
 			
-			holder.populateFrom( model.get( position ) );
+			Restaurant r = model.get( position );
+			
+			(( TextView )row.findViewById( R.id.title )).setText( r.getName() );
+			(( TextView )row.findViewById( R.id.address )).setText( r.getAddress() );
+			
+			ImageView icon = ( ImageView )row.findViewById( R.id.icon );
+			
+			if( r.getType().equals("sit_down")) {
+				icon.setImageResource( R.drawable.ball_red);
+			}
+			else if( r.getType().equals("take_out")) {
+				icon.setImageResource( R.drawable.ball_yellow );
+			}
+			else {
+				icon.setImageResource( R.drawable.ball_green);
+			}
 			
 			return( row );
 			}		
-		}
-	
-	static class RestaurantHolder {
-		private TextView name = null;
-		private TextView address = null;
-		private ImageView icon = null;
-		
-		RestaurantHolder( View row ) {
-			name = ( TextView )row.findViewById( R.id.title );
-			address = ( TextView )row.findViewById( R.id.address );
-			icon = ( ImageView )row.findViewById( R.id.icon );
-		}
-		
-		void populateFrom( Restaurant r ) {
-			name.setText( r.getName() );
-			address.setText( r.getAddress() );
-			
-			if( r.getType().equals( "sit_down") ) {
-				icon.setImageResource( R.drawable.ball_red );
-				name.setTextColor(Color.GREEN);
-				address.setTextColor(Color.GREEN);
-			}
-			else if( r.getType().equals( "take_out" ) ) {
-				icon.setImageResource( R.drawable.ball_yellow );
-				name.setTextColor(Color.BLUE);
-				address.setTextColor(Color.BLUE);
-			}
-			else {
-				icon.setImageResource( R.drawable.ball_green );
-				name.setTextColor(Color.CYAN);
-				address.setTextColor(Color.CYAN);
-			}
-			}
 		}
 	}
