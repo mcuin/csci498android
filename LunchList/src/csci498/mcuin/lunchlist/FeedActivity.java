c6b412fead868ef1ddedf59b111157c0cde6082e
Mykal Cuin <mcuin@mines.edu>
2012-09-30 21:55:51 -0600

    Changed FeedActivity to a much simpler implementation using RSSFeed. Completed step 7 of APT 15.

--- LunchList/src/csci498/mcuin/lunchlist/FeedActivity.java
+++ LunchList/src/csci498/mcuin/lunchlist/FeedActivity.java
@@ -6,6 +6,8 @@ import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.mcsoxford.rss.RSSFeed;
+import org.mcsoxford.rss.RSSReader;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -13,53 +15,6 @@ import android.os.AsyncTask;
 import android.util.Log;
 
 public class FeedActivity extends Activity {
-	
-	private static class FeedTask extends AsyncTask< String, Void, Void > {
-		private Exception e = null;
-		private FeedActivity activity = null;
-		
-		FeedTask( FeedActivity activity ) {
-			attach( activity );
-		}
-		
-		@Override
-		public Void doInBackground( String... urls ) {
-			
-			Properties systemSettings = System.getProperties();
-			
-			systemSettings.put( "http.proxyHost", "your.proxy.host.here" );
-			systemSettings.put( "http.proxyPort", "8080" );
-			
-			try {
-				
-				DefaultHttpClient client = new DefaultHttpClient();
-				HttpGet getMethod = new HttpGet( urls[0] );
-				ResponseHandler< String > responseHandler = new BasicResponseHandler();
-				String responseBody = client.execute( getMethod, responseHandler );
-				
-				Log.d( "FeedActivity", responseBody );
-				
-			}
-			
-			catch ( Exception e ) {
-				
-				this.e = e;
-				
-			}
-			
-			return null;
-			
-		}
-		
-		@Override
-		public void onPostExecute( Void unused ) {
-			if ( e == null ) {
-				Log.e( "LunchList", "Exception parsing feed", e );
-				activity.goBlooey( e );
-				
-			}
-		}
-	}
 	
 	private void goBlooey( Throwable t ) {
 		AlertDialog.Builder builder = new AlertDialog.Builder( this );
@@ -70,6 +25,58 @@ public class FeedActivity extends Activity {
 			.setPositiveButton( "Ok", null );
 			.show();
 		
+	}
+	
+	private static class FeedTask extends AsyncTask< String, Void, RSSFeed > {
+		private Exception e = null;
+		private FeedActivity activity = null;
+		private RSSReader reader = new RSSReader();
+		
+		FeedTask( FeedActivity activity ) {
+			attach( activity );
+		}
+		
+		void attach( FeedActivity activity ) {
+			this.activity = activity;
+		}
+		
+		void detach() {
+			this.activity = null;
+		}
+		
+		@Override
+		public RSSFeed doInBackground( String... urls ) {
+			RSSFeed result = null;
+			
+			Properties systemSettings = System.getProperties();
+			
+			systemSettings.put( "http.proxyHost", "your.proxy.host.here" );
+			systemSettings.put( "http.proxyPort", "8080" );
+			
+			try {
+				
+				result = reader.load( urls[0] );
+				
+			}
+			
+			catch ( Exception e ) {
+				
+				this.e = e;
+				
+			}
+			
+			return result;
+			
+		}
+		
+		@Override
+		public void onPostExecute( RSSFeed feed ) {
+			if ( e == null ) {
+				Log.e( "LunchList", "Exception parsing feed", e );
+				activity.goBlooey( e );
+				
+			}
+		}
 	}
 
 }
