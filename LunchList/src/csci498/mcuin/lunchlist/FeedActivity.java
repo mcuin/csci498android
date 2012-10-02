8bcd33966ef6dc06210c605a8fa2cc45b159eb22
Mykal Cuin <mcuin@mines.edu>
2012-10-01 23:27:21 -0600

    Code matches code given at end of APT 15 but errors on the builder and setAdapter.

--- LunchList/src/csci498/mcuin/lunchlist/FeedActivity.java
+++ LunchList/src/csci498/mcuin/lunchlist/FeedActivity.java
@@ -7,14 +7,65 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.mcsoxford.rss.RSSFeed;
+import org.mcsoxford.rss.RSSItem;
 import org.mcsoxford.rss.RSSReader;
 
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 public class FeedActivity extends Activity {
+	private InstanceState state = null;
+	public static final String FEED_URL = "csci498.mcuin.lunchlist.FEED_URL";
+	
+	@Override
+	public void onCreate( Bundle savedInstanceState ) {
+		super.onCreate( savedInstanceState );
+		
+		state = (InstanceState)getLastNonConfigurationInstance();
+		
+		if ( state == null ) {
+			state = new InstanceState();
+			state.task = new FeedTask( this );
+			state.task.execute( getIntent().getStringExtra( FEED_URL ) );
+			
+		}
+		else {
+			if ( state.task != null ) {
+				state.task.attach( this );
+				
+			}
+			
+			if ( state.feed != null ) {
+				setFeed( state.feed );
+				
+			}
+		}
+	}
+	
+	@Override
+	public Object onRetainNonConfigurationInstance() {
+		if ( state.task != null ) {
+			state.task.detach();
+			
+		}
+		
+		return state;
+		
+	}
+	
+	private void setFeed( RSSFeed feed ) {
+		state.feed = feed;
+		
+		setListAdapter( new FeedAdapter( feed ) );
+	}
 	
 	private void goBlooey( Throwable t ) {
 		AlertDialog.Builder builder = new AlertDialog.Builder( this );
@@ -78,5 +129,57 @@ public class FeedActivity extends Activity {
 			}
 		}
 	}
+	
+	private class FeedAdapter extends BaseAdapter {
+		RSSFeed feed = null;
+		
+		FeedAdapter( RSSFeed feed ) {
+			super();
+			
+			this.feed = feed;
+			
+		}
+		
+		public int getCount() {
+			return feed.getItems().size();
+			
+		}
+		
+		public Object getItem( int position ) {
+			return feed.getItems().get(position);
+			
+		}
+		
+		public long getItemId( int position ) {
+			return position;
+			
+		}
+		
+		public View getView( int position, View convertView, ViewGroup parent ) {
+			View row = convertView;
+			
+			if ( row == null ) {
+				LayoutInflater inflater = getLayoutInflater();
+				
+				row = inflater.inflate( android.R.layout.simple_list_item_1, parent, false );
+				
+			}
+			
+			RSSItem item = ( RSSItem )getItem( position );
+			
+			( ( TextView ) row ).setText( item.getTitle() );
+			
+			return( row );
+			
+		}
+	}
+	
+	private static class InstanceState {
+		RSSFeed feed = null;
+		FeedTask task = null;
+		
+	}
+	
+	
 
 }
