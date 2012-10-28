810afa97e0887c3b4e4b2fb80bc9fb10af46239f
Mykal Cuin <mcuin@mines.edu>
2012-10-28 13:24:11 -0600

    Receiver is set up, and permissions have been added. Completed steps 2 and 3 of APT 19.

---
+++ LunchList/src/csci498/mcuin/lunchlist/OnBootReciever.java
@@ -0,0 +1,15 @@
+package csci498.mcuin.lunchlist;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class OnBootReciever extends BroadcastReceiver {
+
+	@Override
+	public void onReceive( Context ctxt, Intent intent ) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
