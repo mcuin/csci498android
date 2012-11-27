a1c713ae7ffa5821c117c6fa2d71e2a064c71f7e
Mykal Cuin <mcuin@mines.edu>
2012-11-27 00:30:58 -0700

    Updated LunchList for fragment use. Completed step 4 of APT 24 and APT 24.

--- LunchList/src/csci498/mcuin/lunchlist/LunchList.java
+++ LunchList/src/csci498/mcuin/lunchlist/LunchList.java
@@ -3,6 +3,8 @@ package csci498.mcuin.lunchlist;
 import android.os.Bundle;
 import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 
 public class LunchList extends FragmentActivity implements LunchFragment.OnRestaurantListener {
     public final static String ID_EXTRA = "csci498.mcuin.lunchlist._ID";
@@ -23,7 +25,22 @@ public class LunchList extends FragmentActivity implements LunchFragment.OnResta
     	i.putExtra( ID_EXTRA, String.valueOf( id ) );
     	startActivity( i );
     } else {
+    	FragmentManager fragMgr = getSupportFragmentManager();
+    	DetailFragment details = ( DetailFragment )fragMgr.findFragmentById( R.id.details );
     	
+    	if ( details == null ) {
+    		details = DetailFragment.newInstance( id );
+    		
+    		FragmentTransaction xaction = fragMgr.beginTransaction();
+    		
+    		xaction
+    			.add( R.id.details, details );
+    			.setTransition( FragmentTransaction.TRANSIT_FRAGMENT_OPEN );
+    			.addToBackStack( null );
+    			.commit();
+    	} else {
+    		details.loadRestaurant( String.valueOf( id ) );
+    	}
     }
     }
 }
