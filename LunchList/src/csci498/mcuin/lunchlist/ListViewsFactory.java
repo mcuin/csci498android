6f831e3d89cdf3bc4084e5fd47960c1d42051e56
Mykal Cuin <mcuin@mines.edu>
2012-11-26 19:09:28 -0700

    Added a blank preview image as the emulator is too slow to even get a real image from the app. Completed APT 22.

--- LunchList/src/csci498/mcuin/lunchlist/ListViewsFactory.java
+++ LunchList/src/csci498/mcuin/lunchlist/ListViewsFactory.java
@@ -18,26 +18,22 @@ public class ListViewsFactory implements RemoteViewsService.RemoteViewsFactory {
 	public ListViewsFactory( Context ctxt, Intent intent ) {
 		this.ctxt = ctxt;
 	}
-	
-	@Override
+
 	public void onCreate() {
 		helper = new RestaurantHelper( ctxt );
 		restaurants = helper.getReadableDatabase().rawQuery( "Select _ID, name FROM" +
 				"restaurants", null );
 	}
 	
-	@Override
 	public void onDestroy() {
 		restaurants.close();
 		helper.close();
 	}
 	
-	@Override
 	public int getCount() {
 		return restaurants.getCount();
 	}
 	
-	@Override
 	public RemoteViews getViewAt( int position ) {
 		RemoteViews row = new RemoteViews( ctxt.getPackageName(), R.layout.widget_row );
 		Intent i = new Intent();
@@ -45,36 +41,31 @@ public class ListViewsFactory implements RemoteViewsService.RemoteViewsFactory {
 		
 		restaurants.moveToPosition( position );
 		row.setTextViewText( android.R.id.text1, restaurants.getString( 1 ) );
-		extras.putString( LunchList.ID_EXTRA. String.valueOf( restaurants.getInt( 0 ) ) );
+		extras.putString( LunchList.ID_EXTRA, String.valueOf( restaurants.getInt( 0 ) ) );
 		i.putExtras( extras );
 		row.setOnClickFillInIntent( android.R.id.text1, i );
 		
 		return row;
 	}
 	
-	@Override
 	public RemoteViews getLoadingView() {
 		return null;
 	}
 	
-	@Override
 	public int getViewTypeCount() {
 		return 1;
 	}
 	
-	@Override
 	public long getItemId( int position ) {
 		restaurants.moveToPosition( position );
 		
 		return restaurants.getInt( 0 );
 	}
 	
-	@Override
 	public boolean hasStableIds() {
 		return true;
 	}
 	
-	@Override
 	public void onDataSetChanged() {
 		
 	}
